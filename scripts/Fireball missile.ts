fc2076404046c4789d2dbe2d5fa73e8709904db7
Tucnak28 <Tucnak28@email.cz>
2024-03-29 20:34:25 +0100

    Fireball missile is now flamethrower

--- scripts/Fireball missile.ts
+++ scripts/Fireball missile.ts
@@ -1,16 +1,64 @@
 //The code calculates the vector between the player's position and the block they're looking at, then summons a fireball entity with an explosion power of 1 and a direction corresponding to the calculated vector.
 
-Block = Player.rayTraceBlock(1000, false).getBlockPos()
-Pos = Player.getPlayer().getPos()
-x = Pos.getX()
-y = Pos.getY()
-z = Pos.getZ()
+//Old code
+/*
+let Block = Player.rayTraceBlock(1000, false).getBlockPos()
+let Pos = Player.getPlayer().getPos()
 
-xl = Block.getX()
-yl = Block.getY()
-zl = Block.getZ()
+let x = Pos.getX()
+let y = Pos.getY()
+let z = Pos.getZ()
 
-xv = (xl - x)
-yv = (yl - y)
-zv = (zl - z)
-Chat.say("/summon minecraft:fireball ~ ~ ~ {ExplosionPower:1,direction:[" + xv + "," + yv + "," + zv + "]}")
+let xl = Block.getX()
+let yl = Block.getY()
+let zl = Block.getZ()
+
+let xv = (xl - x)
+let yv = (yl - y)
+let zv = (zl - z)
+
+Chat.say(`/summon minecraft:fireball ~ ~ ~ {ExplosionPower:1,direction:[${xv}, ${yv}, ${zv}]}`)*/
+
+
+
+let power = 5;
+let velocityMultiplier = 2;
+let delay = 2;
+
+
+const reverse = !GlobalVars.getBoolean("Flamethrower");
+
+GlobalVars.putBoolean("Flamethrower", reverse);
+if (reverse) {
+    Chat.log(Chat.createTextBuilder().append("[").withColor(0x7)
+        .append("Flamethrower").withColor(0x5)
+        .append("]").withColor(0x7).append(" enabled").withColor(0xc)
+        .build());
+} else {
+    Chat.log(Chat.createTextBuilder().append("[").withColor(0x7)
+        .append("Flamethrower").withColor(0x5)
+        .append("]").withColor(0x7).append(" disabled").withColor(0xc)
+        .build());
+}
+
+
+function launchFireball(pitch, yaw) {
+    let h = pitch * (Math.PI/180)
+    let i = -yaw * (Math.PI/180)
+    let j = Math.cos(i);
+    let k = Math.sin(i);
+    let l = Math.cos(h);
+    let m = Math.sin(h);
+
+    // Multiply the motion components by the velocityMultiplier
+    let motionX = k * l * 2 * velocityMultiplier;
+    let motionY = -m * 2 * velocityMultiplier;
+    let motionZ = j * l * 2 * velocityMultiplier;
+
+    Chat.say(`/summon fireball ~ ~1 ~ {ExplosionPower:${power},Motion:[${motionX}, ${motionY}, ${motionZ}]}`);
+}
+
+while (GlobalVars.getBoolean("Flamethrower")) {
+    launchFireball(Player.getPlayer().getPitch(), Player.getPlayer().getYaw())
+    Client.waitTick(delay); // wait 1 second (synchronized to client ticks)
+}
