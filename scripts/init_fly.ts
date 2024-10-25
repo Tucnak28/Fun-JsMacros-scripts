18dc642caefbb3b22add3b4e2b3cd872e3bf04f2
Tucnak28 <Tucnak28@email.cz>
2024-10-25 22:23:18 +0200

    Toggle flying

--- scripts/init_fly.ts
+++ scripts/init_fly.ts
@@ -1,5 +1,11 @@
 const player = Player.getPlayer();
+const abilities = player.getAbilities();
+const isFlying = abilities.getFlying();
 
-player.addVelocity(0.0,0.08,0.0);
-//player.addPos(0.0,0.15,0.0);
-player.getAbilities().setFlying(true);
+
+if(isFlying) {
+    abilities.setFlying(false);
+} else {
+    player.addVelocity(0.0,0.08,0.0);
+    abilities.setFlying(true);
+}
