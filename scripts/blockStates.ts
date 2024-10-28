908b89c9e58393b27f7b03b0e6a8fbfbc2bdd604
Tucnak28 <Tucnak28@email.cz>
2024-10-28 13:32:00 +0100

    Full BlockStates Tags

--- scripts/blockStates.ts
+++ scripts/blockStates.ts
@@ -6,25 +6,14 @@ const z = center.getZ();
 
 const block = World.getBlock(x, y, z);
 const blockType = block.getId();
-const blockStates = block.getBlockStateHelper().blocksMovement();
+const blockStates = block.getBlockHelper().getTags();
 const blockName =block.getName();
 
 Chat.log(Chat.createTextBuilder().append("\n[").withColor(0x7)
 .append(blockName).withColor(0x5).append("]").withColor(0x7)
 .build());
 
-//blockStates.forEach(state => {
-    Chat.log(blockStates);
-//});
-
-while(1) {
-    const center = Player.rayTraceBlock(1000, true);
-
-const x = center.getX();
-const y = center.getY();
-const z = center.getZ();
-    Chat.say(`/setblock ${x} ${y} ${z} air`);
-    Time.sleep(10)
+for (const state in blockStates) {
+    Chat.log(blockStates[state]);
 }
-
 
